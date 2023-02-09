ae15753c36e076824563d530a2f8f709a6a6e028
Arutos <Arutos@LAPTOP-HATDS20G>
2023-02-09 13:52:47 +0100

    Ajoutez des fichiers projet.

---
+++ Morpion/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace Morpion
+{
+    partial class Morpion
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Morpion));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblJoueur = new System.Windows.Forms.Label();
+            this.btnPlayAgain = new System.Windows.Forms.Button();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Joueur : ";
+            // 
+            // lblJoueur
+            // 
+            this.lblJoueur.AutoSize = true;
+            this.lblJoueur.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJoueur.Location = new System.Drawing.Point(56, 19);
+            this.lblJoueur.Name = "lblJoueur";
+            this.lblJoueur.Size = new System.Drawing.Size(41, 13);
+            this.lblJoueur.TabIndex = 1;
+            this.lblJoueur.Text = "label2";
+            // 
+            // btnPlayAgain
+            // 
+            this.btnPlayAgain.BackgroundImage = global::Morpion.Properties.Resources._1142347;
+            this.btnPlayAgain.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPlayAgain.Location = new System.Drawing.Point(125, 205);
+            this.btnPlayAgain.Name = "btnPlayAgain";
+            this.btnPlayAgain.Size = new System.Drawing.Size(50, 34);
+            this.btnPlayAgain.TabIndex = 2;
+            this.btnPlayAgain.UseVisualStyleBackColor = true;
+            this.btnPlayAgain.Click += new System.EventHandler(this.btnPlayAgain_Click);
+            // 
+            // lblWin
+            // 
+            this.lblWin.AutoSize = true;
+            this.lblWin.Location = new System.Drawing.Point(12, 213);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(35, 13);
+            this.lblWin.TabIndex = 3;
+            this.lblWin.Text = "label2";
+            // 
+            // Morpion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 248);
+            this.Controls.Add(this.lblWin);
+            this.Controls.Add(this.btnPlayAgain);
+            this.Controls.Add(this.lblJoueur);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Morpion";
+            this.Text = "Morpion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblJoueur;
+        private System.Windows.Forms.Button btnPlayAgain;
+        private System.Windows.Forms.Label lblWin;
+    }
+}
+
---
+++ Morpion/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Morpion
+{
+    public partial class Morpion : Form
+    {
+        int nbClic = 0;
+        List<Button> btnListe = new List<Button>();
+        List<Label> lblListe = new List<Label>();
+        List<int> X = new List<int>();
+        List<int> O = new List<int>();
+        public Morpion()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            lblWin.Visible = false;
+            lblWin.Text = "";
+            CreateButtons();
+            lblJoueur.Text = "1";
+            X.Clear();
+            O.Clear();
+            nbClic = 0;
+        }
+
+        private void CreateButtons()
+        {
+            DeleteButtons();
+            int size = 50;
+            btnListe.Clear();
+            lblListe.Clear();
+            for (int x = 0; x < 3; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+
+                    Label label = new Label();
+                    Controls.Add(label);
+                    label.Visible = false;
+                    label.Text = "";
+                    label.Size = new Size(30, 30);
+                    label.Font = new Font("", 18);
+                    label.Location = new Point(27 + (size + 5) * x, 50 + (size + 5) * y);
+                    lblListe.Add(label);
+
+
+                    Button btn = new Button();
+                    Controls.Add(btn);
+                    btn.Size = new Size(size, size);
+                    btn.Visible = true;
+                    btn.Location = new Point(15 + (size + 5) * x, 40 + (size + 5) * y);
+                    btn.Click += new EventHandler(button_Click);
+                    btnListe.Add(btn);
+                }
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            nbClic++;
+            var indice = btnListe.IndexOf((Button)sender);
+            btnListe[indice].Visible = false;
+            lblListe[indice].Visible = true;
+            if (lblJoueur.Text == "1")
+            {
+                lblListe[indice].Text = "X";
+            }
+            else
+            {
+                lblListe[indice].Text = "O";
+            }
+            if (Winner(indice, lblJoueur.Text))
+            {
+                End("Victoire du joueur " + lblJoueur.Text + " !");
+                PlayerChange();
+            }
+            else
+            {
+                if (nbClic == 9)
+                {
+                    End("Égalité !");
+                }
+            }
+            PlayerChange();
+
+        }
+
+        private void btnPlayAgain_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void PlayerChange()
+        {
+            if (lblJoueur.Text == "1")
+            {
+                lblJoueur.Text = "2";
+            }
+            else
+            {
+                lblJoueur.Text = "1";
+            }
+        }
+        private void DeleteButtons()
+        {
+            for (int k = 0; k < btnListe.Count + lblListe.Count; k++)
+            {
+                this.Controls.RemoveAt(4);
+            }
+        }
+        private void End(string message)
+        {
+            lblWin.Visible = true;
+            lblWin.Text = message;
+            for (int k = 0; k < btnListe.Count; k++)
+            {
+                btnListe[k].Enabled = false;
+                btnListe[k].Visible = false;
+            }
+
+        }
+        private bool Winner(int indice, string Joueur)
+        {
+            //Victoire du joueur 1 (X)//
+            if (lblListe[indice].Text == "X")
+            {
+                X.Add(indice);
+                if (X.Contains(4))
+                {
+                    if (X.Contains(0) && X.Contains(8) || X.Contains(2) && X.Contains(6) || X.Contains(1) && X.Contains(7) || X.Contains(3) && X.Contains(5))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    if (X.Contains(0))
+                    {
+                        if (X.Contains(1) && X.Contains(2) || X.Contains(3) && X.Contains(6))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (X.Contains(8))
+                        {
+                            if (X.Contains(6) && X.Contains(7) || X.Contains(2) && X.Contains(5))
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                O.Add(indice);
+                if (O.Contains(4))
+                {
+                    if (O.Contains(0) && O.Contains(8) || O.Contains(2) && O.Contains(6) || O.Contains(1) && O.Contains(7) || O.Contains(3) && O.Contains(5))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (O.Contains(0))
+                    {
+                        if (O.Contains(1) && O.Contains(2) || O.Contains(3) && O.Contains(6))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        if (O.Contains(8))
+                        {
+                            if (O.Contains(6) && O.Contains(7) || O.Contains(2) && O.Contains(5))
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
